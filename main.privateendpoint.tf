78a2b1bfa77a7cacc6a8f6f6fd277fe663241ca4
Puneet V Devadiga <52238436+puneetdevadiga@users.noreply.github.com>
2025-09-30 23:39:03 +0800

    feat(aro): add profile vars, name validation, subnet fix, private endpoint resource updates (#48)

--- main.privateendpoint.tf
+++ main.privateendpoint.tf
@@ -12,8 +12,10 @@ resource "azurerm_private_endpoint" "this_managed_dns_zone_groups" {
   private_service_connection {
     is_manual_connection           = false
     name                           = each.value.private_service_connection_name != null ? each.value.private_service_connection_name : "pse-${var.name}"
-    private_connection_resource_id = azurerm_resource_group.TODO.id # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
-    subresource_names              = ["TODO subresource name, see https://learn.microsoft.com/en-us/azure/private-link/private-endpoint-overview#private-link-resource"]
+  # NOTE: Replace with correct ARO private link resource once supported; placeholder uses cluster id.
+  private_connection_resource_id = azurerm_redhat_openshift_cluster.this.id
+  # ARO private endpoint subresource names not finalized in scaffold; update when known.
+  subresource_names              = ["cluster"]
   }
   dynamic "ip_configuration" {
     for_each = each.value.ip_configurations
@@ -21,8 +23,8 @@ resource "azurerm_private_endpoint" "this_managed_dns_zone_groups" {
     content {
       name               = ip_configuration.value.name
       private_ip_address = ip_configuration.value.private_ip_address
-      member_name        = "TODO subresource name"
-      subresource_name   = "TODO subresource name"
+  member_name        = "cluster"
+  subresource_name   = "cluster"
     }
   }
   dynamic "private_dns_zone_group" {
@@ -51,8 +53,8 @@ resource "azurerm_private_endpoint" "this_unmanaged_dns_zone_groups" {
   private_service_connection {
     is_manual_connection           = false
     name                           = each.value.private_service_connection_name != null ? each.value.private_service_connection_name : "pse-${var.name}"
-    private_connection_resource_id = azurerm_resource_group.TODO.id # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
-    subresource_names              = ["TODO subresource name, see https://learn.microsoft.com/en-us/azure/private-link/private-endpoint-overview#private-link-resource"]
+  private_connection_resource_id = azurerm_redhat_openshift_cluster.this.id
+  subresource_names              = ["cluster"]
   }
   dynamic "ip_configuration" {
     for_each = each.value.ip_configurations
@@ -60,8 +62,8 @@ resource "azurerm_private_endpoint" "this_unmanaged_dns_zone_groups" {
     content {
       name               = ip_configuration.value.name
       private_ip_address = ip_configuration.value.private_ip_address
-      member_name        = "TODO subresource name"
-      subresource_name   = "TODO subresource name"
+  member_name        = "cluster"
+  subresource_name   = "cluster"
     }
   }
 
--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,6 @@ output "private_endpoints" {
 # Module owners should include the full resource via a 'resource' output
 # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
 output "resource" {
-  description = "This is the full output for the resource."
-  value       = azurerm_resource_group.TODO # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
+  description = "This is the full output for the Red Hat OpenShift cluster resource."
+  value       = azurerm_redhat_openshift_cluster.this
 }
